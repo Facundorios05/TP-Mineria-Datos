ea2be370257ec0cf2fb8dd45ac073d8204af5ae0
FacundoMajda <facundomajda13@gmail.com>
2023-09-10 19:35:49 -0300

    asd

--- middlewares/configRole.js
+++
@@ -1,9 +0,0 @@
-import { ROLES } from "../models/user.model.js";
-
-export const isAdmin = (req, res, next) => {
-  if (req.user.role !== ROLES.ADMIN) {
-    return res.sendStatus(403);
-  }
-
-  next();
-};
